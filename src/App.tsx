cdf8c08e1c1d5a14d249793f5068fc62c96d6d83
SAEZ1205 <saez123m@gmail.com>
2026-08-16 05:30:29 -0500

    Agregar rutas del boceto de asesor

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { FinalInterfaceFrame } from './components/shared/FinalInterfaceFrame'
+import DashboardAsesor from './pages/asesor/DashboardAsesor'
+import CasoDetalle from './pages/asesor/CasoDetalle'
 
 export default function App() {
+  const path = window.location.pathname
+  if (path.startsWith('/asesor/caso/')) return <CasoDetalle />
+  if (path === '/asesor' || path === '/asesor/') return <DashboardAsesor />
   return <FinalInterfaceFrame />
 }
